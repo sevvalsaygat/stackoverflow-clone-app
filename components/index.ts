0caba0a8335b6e6e445d32d8b3cda39f6710be84
sevvalsaygat <sevvallsstt@gmail.com>
2023-04-03 18:27:25 +0300

    fix: pagination

--- components/index.ts
+++ components/index.ts
@@ -2,6 +2,7 @@ import Footer from './Footer';
 import Header from './Header';
 import Icons from './Icons';
 import Buttons from './Buttons';
+import Pagination from './Pagination';
 // import MyEditor from './Editor';
 
-export { Footer, Header, Icons, Buttons };
+export { Footer, Header, Icons, Buttons, Pagination };
